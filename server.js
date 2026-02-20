d7ead41d40ff79e1522ecb67558d4687e34dcfcf
Rahul18A09 <rahulbharda60@gmail.com>
2026-02-20 22:19:22 +0530

    practice

--- server.js
+++ server.js
@@ -11,12 +11,11 @@ let notes = [];
 // create post
 
 app.post("/notes", (req, res) => {
-    const { title, description } = req.body;
+  const { title, description } = req.body;
 
-
- if (!title || !description) {
+  if (!title || !description) {
     return res.status(400).json({ message: "Title and description required" });
-  } 
+  }
 
   const newNote = {
     id: notes.length + 1,
@@ -24,10 +23,77 @@ app.post("/notes", (req, res) => {
     description,
   };
 
-   notes.push(newNote);
+  notes.push(newNote);
 
-    res.status(201).json({
+  res.status(201).json({
     message: "Note created successfully",
     note: newNote,
   });
 });
+
+// Read - Get all notes
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "Notes fetched successfully",
+    notes,
+  });
+});
+
+// Read - Get a single note by ID
+
+app.get("/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const note = notes.find((n) => n.id === id);
+
+  if (!note) {
+    return res.status(404).json({ message: "Note not found" });
+  }
+
+  res.json(note);
+});
+
+// Update - Update a note by ID
+
+app.put("/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const note = notes.find((n) => n.id === id);
+
+  if (!note) {
+    return res.status(404).json({ message: "Note not found" });
+  }
+
+  const { title, description } = req.body;
+
+  if (title) note.title = title;
+  if (description) note.description = description;
+
+  res.json({
+    message: "Note updated successfully",
+    note,
+  });
+});
+
+// Delete - delete notes by id
+
+app.delete("/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = notes.findIndex((n) => n.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "Note not found" });
+  }
+
+  notes.splice(index, 1);
+
+  res.json({message: "Note deleted successfully"});
+
+});
+
+
+// start server
+
+app.listen(PORT,  () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    
+})
