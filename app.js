0560d865e95f48c81bdf1a4d5bcf4465dbac47f8
Rahul18A09 <rahulbharda60@gmail.com>
2026-02-21 12:39:26 +0530

    new

---
+++ app.js
@@ -0,0 +1,32 @@
+// console.log("Hello Rahul, Welcome  to Node.js");
+
+// console.log("start");
+
+// setTimeout(() => {
+//     console.log("This is a timeout function");
+// }, 2000);
+
+// console.log("end");
+
+
+// file system
+const fs = (require("fs"));
+
+fs.writeFileSync("test.txt", "Hello Rahul");
+
+
+
+// HTTP Server
+const http  = require("http");
+
+const server = http.createServer((req, res) => {
+    res.end("Hello From Node.js Server");
+});
+
+server.listen(3000);
+
+
+
+
+
+
