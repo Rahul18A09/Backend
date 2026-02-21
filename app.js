cad9f3448bb033ff3f65a005f4a745512bee4395
Rahul18A09 <rahulbharda60@gmail.com>
2026-02-21 22:20:23 +0530

    update

--- app.js
+++ app.js
@@ -10,20 +10,30 @@
 
 
 // file system
-const fs = (require("fs"));
+// const fs = (require("fs"));
 
-fs.writeFileSync("test.txt", "Hello Rahul");
+// fs.writeFileSync("test.txt", "Hello Rahul");
 
 
 
 // HTTP Server
-const http  = require("http");
+// const http  = require("http");
 
-const server = http.createServer((req, res) => {
-    res.end("Hello From Node.js Server");
+// const server = http.createServer((req, res) => {
+//     res.end("Hello From Node.js Server");
+// });
+
+// server.listen(3000);
+
+
+const express  = require("express");
+const app  = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello Rahul 🚀")
 });
 
-server.listen(3000);
+app.listen(3000, () => console.log("Server running"));
 
 
 
