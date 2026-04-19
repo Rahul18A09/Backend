45ce98aba74622463c0bf2d062ab266f471d6926
Rahul18A09 <rahulbharda60@gmail.com>
2026-04-19 13:39:37 +0530

    complete mern app

--- img-caption-app/Frontend/img-caption/src/pages/CreatePost.jsx
+++ img-caption-app/Frontend/img-caption/src/pages/CreatePost.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 const CreatePost = () => {
+
+const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+
+        const formData = new FormData(e.target)
+
+        axios.post("http://localhost:5000/create-post", formData)
+        .then((res) =>  {
+
+            alert("Post Created successfully")
+            e.target.reset()
+            // console.log(res);
+
+            navigate("/feed");
+            
+        })
+        .catch ((err) => {
+            console.log("Error:", err);
+            alert("Error Creating Post")
+            
+        })
+
+    }
+
+
   return (
     <section className="create-post-section">
       <h1>Create Post</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="file"  name='image' accept="image/*" />
         <input type="text" name="caption" placeholder="Enter Caption" required />
         <button type="submit">Submit</button>
--- img-caption-app/Frontend/img-caption/src/pages/Feed.jsx
+++ img-caption-app/Frontend/img-caption/src/pages/Feed.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([
-        {
-            _id:'1',
-            image:'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-            caption:'A beautiful view of Paris'
-        }
-    ]);
-
+  useEffect(() => {
+    axios.get("http://localhost:5000/posts").then((res) => {
+      console.log(res.data);
+      setPosts(res.data.post || []);
+    });
+  }, []);
 
   return (
-    <section className='feed-section'>
-        {
-            posts.length > 0 ? (
-                posts.map((post) => (
-                    <div className='post-card' key={post._id}>   
-                        <img src={post.image} alt={post.caption} />
-                        <p>{post.caption}</p>
-                    </div>      
-
-                ))
-            ) : (
-                <h1>No posts available</h1>
-            )
-        }
+    <section className="feed-section">
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <div className="post-card" key={post._id}>
+            <img src={post.image} alt={post.caption} />
+            <p>{post.caption}</p>
+          </div>
+        ))
+      ) : (
+        <h1>No posts available</h1>
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
